a8cee01ce52742d3e5061ac57992106b2370c5f1
Prisha1611 <prishapatel16@gmail.com>
2024-02-20 19:56:34 +0300

    added registration and login page

---
+++ app/src/main/java/com/example/forest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.forest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button getStartedButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getStartedButton = findViewById(R.id.getStartedButton);
+
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/forest/login.java
@@ -0,0 +1,28 @@
+package com.example.forest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login extends AppCompatActivity {
+    Button registerButton, loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        registerButton = findViewById(R.id.registerButton);
+        loginButton = findViewById(R.id.loginButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
